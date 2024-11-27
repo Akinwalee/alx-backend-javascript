5aad4d20023e8e0fa6863f4e5df01647bbfa6f9e
Obatula Fuad <obatulafuad@gmail.com>
2024-11-27 05:37:29 +0100

    Task 1

---
+++ 0x05-Node_JS_basic/0-concole.js
@@ -0,0 +1,10 @@
+/**
+ * Displays message on STDOUT.
+ * @param {String} msg The message to display.
+ * @author Obatula Fuad <https://github.com/Akinwalee>
+ */
+const displayMessage = (msg) => {
+    console.log(msg);
+  };
+  
+  module.exports = displayMessage;
